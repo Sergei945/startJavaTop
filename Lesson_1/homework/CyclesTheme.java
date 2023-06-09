1f964d33d64ae727b3c2cbbef0d35631853fcdbc
Avel945 <fedor945@yandex.ru>
2023-06-09 22:25:18 +0300

    six exx CyclesTheme

--- Lesson_1/homework/CyclesTheme.java
+++ Lesson_1/homework/CyclesTheme.java
@@ -66,25 +66,27 @@ class CyclesTheme {
         num1 = 1;
         num2 = 30;
         count = 1;
-        for (int i = num1; i < num2 - 1; i += 2) {
-            int length = (int) (Math.log10(i) + 1);
-            System.out.printf("%3d", i);
+        int spaceRight = 3;
+        int length = (int) (Math.log10(num1 + num2) + 2);
+        for (int i = num1; i < num2; i += 2) {
+            System.out.printf("%" + (length) +"d", i);
             if(count % 5 == 0) {
-                System.out.printf("%n", "");
+                System.out.println("");
             }
             count++;
-        }
-        if(((num2 - 1) / 2 % 5) % 5 == 4) {
-            System.out.printf("%" + 3 + "d", 0);
-        } else if(((num2 - 1) / 2 % 5) == 3) {
-            System.out.printf("%3d%3d", 0, 0);
-        } else if(((num2 - 1) / 2 % 5) == 2) {
-            System.out.printf("%3d%3d%3d", 0, 0, 0);
-        } else if(((num2 - 1) / 2 % 5) == 1) {
-            System.out.printf("%3d%3d%3d%3d", 0, 0, 0, 0);
+        }   
+        if((count - 1) % 5 == 4) {
+            System.out.printf("%" + length + "d", 0);
+        } else if((count - 1) % 5 == 3) {
+            System.out.printf("%" + length + "d%" + length + "d", 0, 0);
+        } else if((count - 1) % 5 == 2) {
+            System.out.printf("%" + length + "d%" + length + "d%" + length + "d", 0, 0, 0);
+        } else if((count - 1) % 5 == 1) {
+            System.out.printf("%" + length + "d%" + length +
+                "d%" + length + "d%" + length + "d", 0, 0, 0, 0);
         }
 
-        System.out.println("\n\n\n5. Проверка количества двоек на четность/нечетность\n");
+        System.out.println("\n\n5. Проверка количества двоек на четность/нечетность\n");
 
         int num = 3242592;
         System.out.print("число " + num);
@@ -98,6 +100,50 @@ class CyclesTheme {
         }
         System.out.print(" содержит " + countDigitTwo +
             ((countDigitTwo % 2 == 0) ? " четных" : " нечетных")  + " двоек");
+        
         System.out.println("\n\n\n6. Отображение фигур в консоли\n");
+
+        char asterisk = '*';
+        for (int i = 1; i <= 50; i++) {
+            System.out.print(asterisk);
+            if(i % 10 == 0) {
+                System.out.println("");
+            }
+        }
+        System.out.println();
+
+        char pound = '#';
+        int i = 5;
+        while(i > 0) {
+            int j = i;
+            while(j > 0) {
+                System.out.print(pound);
+                j--;
+            }
+            System.out.println("");
+            i--;
+        }
+        System.out.println();
+        
+        char dollarSign = '$';
+        i = 1;
+        int lines = 5;
+        double middle = ((double) lines ) / 2;
+        int asterisks = 1;
+        do {
+            int j = 0;
+            do {
+                System.out.print(dollarSign);
+                j++;
+            } while (j < asterisks);
+            if(i < middle) {
+                asterisks += 2;
+            } else if(j > middle) {
+                asterisks -= 2;
+            }
+            i++;
+            System.out.println();
+        } while(i <= lines);
+
     }
 }
