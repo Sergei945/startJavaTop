34febba131ea433f3a7e8370a44a207723de2f7a
Avel945 <fedor945@yandex.ru>
2023-06-14 20:58:59 +0300

    I hope this my finish commit CyclesTheme

--- Lesson_1/homework/CyclesTheme.java
+++ Lesson_1/homework/CyclesTheme.java
@@ -32,16 +32,16 @@ class CyclesTheme {
         System.out.println("\n\n\n3. Вывод реверсивного числа и суммы его цифр\n");
 
         int num = 1234;
-        String strNum = num + "";
+        int copyNum = num;
         int sum = 0;
-        System.out.println("Число в обратном порядке:");
-        while(num < 0) {
-            int digit = num % 10;
-            System.out.print(digit + " ");
+        int reverseNum = 0;
+        while(copyNum > 0) {
+            int digit = copyNum % 10;
+            copyNum /= 10;
+            reverseNum = reverseNum * 10 + digit;
             sum += digit;
-            num /= 10;
         }
-        System.out.println("сумма его цифр - " + sum);
+        System.out.println("Число в обратном порядке: " + reverseNum + " сумма его цифр - " + sum);
 
         System.out.println("\n\n4. Вывод чисел на консоль в несколько строк\n");
 
@@ -65,7 +65,7 @@ class CyclesTheme {
         System.out.println("\n\n5. Проверка количества двоек на четность/нечетность\n");
 
         num = 3242592;
-        int copyNum = num; 
+        copyNum = num; 
         int countTwos = 0;
         while(copyNum > 0) {
             if(copyNum % 10 == 2) {
@@ -140,48 +140,38 @@ class CyclesTheme {
 
         num = 1234321;
         copyNum  = num;
-        int reverseNum = 0;
-        boolean isPalindrom = true;
-
+        reverseNum = 0;
         while(copyNum > 0) {
             int digit = copyNum % 10;
             copyNum /= 10;
-            reverseNum *= 10;
-            reverseNum += digit;
+            reverseNum = reverseNum * 10 + digit;
         }
-        for (int i = num; i > 0;i /= 10) {
-            if(i % 10 != reverseNum % 10) {
-               isPalindrom = false;
-            }
-            reverseNum /= 10;
-        }
-        System.out.println((isPalindrom)
+        System.out.println((reverseNum == num)
                 ? "Число " + num + " палиндром" : "Число " + num + " не палиндром");
 
         System.out.println("\n\n9. Определение, является ли число счастливым\n");
 
         num = 123321;
+        copyNum = num;
         int sumLeft = 0;
         int sumRight = 0;
-        length = (int) (Math.log10(num) + 1);
-        System.out.println("Определяем счастливое ли число - " + num);
-        for (int i = length; i >= 1; i--) {
-            if(i > 0 && i < 4) {
+        for (int i = 6; i <= 6; i--) {
+            if(i <= 3) {
                 sumLeft += num % 10;
-            } else if(i <= length && i > length - 3) {
+            } else {
                 sumRight += num % 10;
             }
             num /= 10;
         }
-        System.out.println("Сумма первых 3 цифр - " + sumLeft + "\n" +
-                "Сумма последних 3 цифр - " + sumRight + "\n" + 
+        System.out.println("Определяем счастливое ли число - " + num + "\nСумма первых 3 цифр - "
+                 + sumLeft + "\n" + "Сумма последних 3 цифр - " + sumRight + "\n" + 
                 ((sumRight == sumLeft) ? "Числа равны число счастливое" : "Число не счастливое"));
 
         System.out.println("\n\n10. Вывод таблицы умножения Пифагора\n");
 
         System.out.println("                ТАБЛИЦА ПИФАГОРА\n");
-        for (int i = 1; i < 11;i++) {
-            for (int j = 1;j < 11;j++) {
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j++) {
                 if(i == 1 && j == 1) {
                     System.out.printf("%4s", " ");
                 } else {
@@ -193,7 +183,7 @@ class CyclesTheme {
             }
             System.out.println();
             if(i == 1) {
-                for (int k = 0;k < 11;k++) {
+                for (int k = 0; k < 11; k++) {
                     System.out.printf("%4s", "____");
                 }
                 System.out.println();
