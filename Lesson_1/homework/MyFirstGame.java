5937f097fe0d9805f1a1688e1b392e5cbe67946d
Avel945 <fedor945@yandex.ru>
2023-06-16 22:39:22 +0300

    lesson one fixing less bags

--- Lesson_1/homework/MyFirstGame.java
+++ Lesson_1/homework/MyFirstGame.java
@@ -6,35 +6,33 @@ class MyFirstGame {
         int min = 1;
         int max = 100;
         boolean isMore = false;
-
         System.out.println("Игра началась угадайте число, которое загадал компьютер" +
                 " в диапазоне от 1 до 100");
         while(playerNum != secretNum) {
             if(playerNum < 1 || playerNum > 100) {
                 System.out.println("Ошибка число (" + playerNum + ") не в диапазоне от 1 до 100");
                 return;
+            } 
+            if(playerNum < secretNum) {
+                System.out.println("число такое-то больше того, что загадал компьютер");
+                isMore = true;
             } else {
-                if(playerNum < secretNum) {
-                    System.out.println("число такое-то больше того, что загадал компьютер");
-                    isMore = true;
-                } else {
-                    System.out.println("число такое-то меньше того, что загадал компьютер");
-                    isMore = false;
-                }
-                if(isMore) {
-                    min += (max - min) / 2;
-                    if((max - min) / 2 != 0) {
-                        playerNum += (max - min) / 2;
-                    } else {
-                        playerNum++;
-                    }
-                } else {
-                    max -= (max - min) / 2;
-                    playerNum -= (max - min) / 2;
-                }
-                System.out.println("Я думаю загаданное число - " + playerNum);
+                System.out.println("число такое-то меньше того, что загадал компьютер");
+                isMore = false;
             }
-            
+            int maxMinusMinDivTwo = (max - min) / 2;
+            if(isMore) {
+                min += maxMinusMinDivTwo;
+                if(maxMinusMinDivTwo != 0) {
+                    playerNum += maxMinusMinDivTwo;
+                } else {
+                    playerNum++;
+                }
+            } else {
+                max -= maxMinusMinDivTwo;
+                playerNum -= maxMinusMinDivTwo;
+            }
+            System.out.println("Я думаю загаданное число - " + playerNum);
         }
         System.out.println("Поздравляю вы выиграли");
     }
