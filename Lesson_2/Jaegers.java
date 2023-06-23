336959c1be6afa35f7092ce556a541d5c09cc98c
Sergey <fedor945@yandex.ru>
2023-06-23 11:27:33 +0300

    Jaegers fix

--- Lesson_2/Jaegers.java
+++ Lesson_2/Jaegers.java
@@ -10,117 +10,77 @@ class Jaegers {
     private int armor;
     private int kaijuKilled;
 
-    public Jaegers(String name, String mark, String status, String origin, int height, int weight,
-            int speed, int strength, int armor, int kaijuKilled) {
+    public Jaegers() {
+
+    }
+
+    public Jaegers(String name, String mark, String status, String origin, int height, 
+            int weight, int speed, int strength, int armor, int kaijuKilled) {
         this.name = name;
         this.mark = mark;
         this.status = status;
         this.origin = origin;
+        setHeight(height);
+        setWeight(weight);
+        setSpeed(speed);
+        setStrength(strength);
+        setArmor(armor);
+    }
 
-        if(weight > 0) {
-            this.weight = weight;
-        } else {
-            System.out.println("Вес не может быть меньше нуля");
-        }
-        if(speed > 0) {
-            this.speed = speed;
-        } else {
-            System.out.println("Скорость не может быть меньше нуля");
-        }
+    public void setHeight(int height) {
         if(height > 0) {
             this.height = height;
         } else {
             System.out.println("Высота не может быть меньше нуля");
         }
+    }
+
+    public void setWeight(int weight) {
+        if(weight > 0) {
+            this.weight = weight;
+        } else {
+            System.out.println("Вес не может быть меньше нуля");
+        }
+    }
+
+    public void setSpeed(int speed) {
+        if(speed > 0) {
+            this.speed = speed;
+        } else {
+            System.out.println("Скорость не может быть меньше нуля");
+        }
+    }
+
+    public void setStrength(int strength) {
         if(strength > 0) {
             this.strength = strength;
         } else {
             System.out.println("Сила не может быть меньше нуля");
         }
-        if(speed > 0) {
+    }
+
+    public void setArmor(int armor) {
+        if(armor > 0) {
             this.armor = armor;
         } else {
             System.out.println("Броня не может быть меньше нуля");
         }
-    }
-    public Jaegers() {
-
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
-    public int getArmor() {
-        return armor;
-    }
-
-    public void setArmor(int armor) {
-        this.armor = armor;
-    }
-
-    public int getKaijuKilled() {
-        return kaijuKilled;
     }
 
     public void setKaijuKilled(int kaijuKilled) {
         this.kaijuKilled = kaijuKilled;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getMark() {
-        return mark;
     }
 
     public void setMark(String mark) {
         this.mark = mark;
     }
 
-    public String getStatus() {
-        return status;
-    }
-
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public String getOrigin() {
-        return origin;
     }
 
     public void setOrigin(String origin) {
@@ -128,14 +88,27 @@ class Jaegers {
     }
 
     public void go() {
-        System.out.println(name + " идёт к цели");
+        System.out.println(name + " идёт к цели\n");
     }
 
     public void scan() {
-        System.out.println(name + "сканирует цель");
+        System.out.println(name + "сканирует цель\n");
     }
 
     public void hit() {
-        System.out.println(name + " бьёт цель и наносит " + strength + " урона");
+        System.out.println(name + " бьёт цель и наносит " + strength + " урона\n");
+    }
+
+    public String toString() {
+        return ("Егерь по имени " + name + 
+                " имеет следующие характеристики:\n" +
+                "Происхождение - " + origin + "\n" +
+                "Марка - " + mark + "\n" +
+                "Статус - " + status + "\n" +
+                "Вес - " + weight + "\n" +
+                "Скорость - " + speed + "\n" +
+                "Высота - " + height + "\n" +
+                "Сила - " + strength + "\n" +
+                "Броня - " + armor + "\n");
     }
 }
--- Lesson_2/JaegersTest.java
+++ Lesson_2/JaegersTest.java
@@ -1,10 +1,8 @@
 class JaegersTest {
 
     public static void main(String[] args) {
-        Jaegers gipsyDanger  = new Jaegers("Gipsy Danger", "Mark-3", 
-                "Destroyed", "USA", 260, 1980, 7, 8, 6, 9);
-        Jaegers chernoAlpha   = new Jaegers();
-
+        
+        Jaegers chernoAlpha = new Jaegers();
         chernoAlpha.setName("Cherno Alpha");
         chernoAlpha.setOrigin("Russia");
         chernoAlpha.setMark("Mark-1");
@@ -14,26 +12,17 @@ class JaegersTest {
         chernoAlpha.setHeight(280);
         chernoAlpha.setStrength(10);
         chernoAlpha.setArmor(10);
-        printInfoJeagers(gipsyDanger);
-        printInfoJeagers(chernoAlpha);
-        gipsyDanger.go();
-        gipsyDanger.scan();
-        gipsyDanger.hit();
+        System.out.println(chernoAlpha.toString());
         chernoAlpha.go();
         chernoAlpha.scan();
         chernoAlpha.hit();
+
+        Jaegers gipsyDanger  = new Jaegers("Gipsy Danger", "Mark-3", 
+                "Destroyed", "USA", 260, 1980, 7, 8, 6, 9);
+        System.out.println(gipsyDanger.toString());
+        gipsyDanger.go();
+        gipsyDanger.scan();
+        gipsyDanger.hit();
     }
 
-    public static void printInfoJeagers(Jaegers jaeger) {
-        System.out.println("Егерь по имени " + jaeger.getName() + 
-                " имеет следующие характеристики:\n" +
-                "Происхождение - " + jaeger.getOrigin() + "\n" +
-                "Марка - " + jaeger.getMark() + "\n" +
-                "Статус - " + jaeger.getStatus() + "\n" +
-                "Вес - " + jaeger.getWeight() + "\n" +
-                "Скорость - " + jaeger.getSpeed() + "\n" +
-                "Высота - " + jaeger.getHeight() + "\n" +
-                "Сила - " + jaeger.getStrength() + "\n" +
-                "Броня - " + jaeger.getArmor() + "\n");
-    }
 }
