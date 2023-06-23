6b406cc184723b42d7f1a52ff0f510f0a89d8ad0
Sergey <fedor945@yandex.ru>
2023-06-23 11:53:47 +0300

    fix toString

--- Lesson_2/JaegersTest.java
+++ Lesson_2/JaegersTest.java
@@ -12,14 +12,14 @@ class JaegersTest {
         chernoAlpha.setHeight(280);
         chernoAlpha.setStrength(10);
         chernoAlpha.setArmor(10);
-        System.out.println(chernoAlpha.toString());
+        System.out.println(chernoAlpha);
         chernoAlpha.go();
         chernoAlpha.scan();
         chernoAlpha.hit();
 
         Jaegers gipsyDanger  = new Jaegers("Gipsy Danger", "Mark-3", 
                 "Destroyed", "USA", 260, 1980, 7, 8, 6, 9);
-        System.out.println(gipsyDanger.toString());
+        System.out.println(gipsyDanger);
         gipsyDanger.go();
         gipsyDanger.scan();
         gipsyDanger.hit();
