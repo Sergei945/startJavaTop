c4905fc834e6f21de51d173049f2f154f1015397
Avel945 <fedor945@yandex.ru>
2023-07-21 21:27:41 +0300

    cool view Guess new version

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -3,8 +3,6 @@ package com.startjava.lesson_2_3_4.guess;
 import java.util.Scanner;
 
 class GuessNumber {
-
-    Scanner console = new Scanner(System.in, "cp866");
     private int secretNum;
     private Player player1;
     private Player player2;
@@ -15,49 +13,57 @@ class GuessNumber {
     }
 
     public void play() {
-
         System.out.println("Игра началась. Компьютер загадал число от 1 до 100. Отгадайте быстрее соперника\n" +
                 "У каждого игрока по 10 попыток");
         randomNum();
-        System.out.println(secretNum);
-        while (isGuessed(player1) && isGuessed(player2)) { }
+        while (isGuessed(player1) && isGuessed(player2)) {
+        }
         printNums(player1);
         printNums(player2);
         player2.clear();
         player1.clear();
     }
 
+    private void randomNum() { secretNum = (int) (Math.random() * 100) + 1; }
+
     public boolean isGuessed(Player player) {
-        if(player.getNum() == secretNum) {
-            System.out.println("Игрок " + player.getName() +
-                    " угадал число " + secretNum + " с " + (player.getIndexArray() + 1) + " попытки");
+        Scanner console = new Scanner(System.in, "cp866");
+        if (player.getAttempt() == 10) {
+            System.out.println("Вы проиграли ваши количество попыток 0");
             return false;
         }
-        System.out.print(player.getName() + " введите число от 1 до 100 ");
-        player.addNum(console.nextInt());
-        if(player.getNum() > secretNum) {
-            System.out.println("Число " + player.getNum() + " игрока " + player.getName() + 
+       int playerNum;
+        do{
+            System.out.print(player.getName() + " введите число от 1 до 100 ");
+            playerNum = console.nextInt();
+            if(playerNum < 1 || playerNum > 100) {
+                System.out.println("Ошибка введено число меньше 1 или больше 100 попробуйте снова");
+            }
+        } while (playerNum < 1 || playerNum > 100);
+        player.addNum(playerNum);
+        if (playerNum > secretNum) {
+            System.out.println("Число " + playerNum + " игрока " + player.getName() +
                     " больше того, что загадал компьютер\n" +
-                    "Количество попыток осталось - " + (9 - player.getIndexArray()));
-        } else if(player.getNum() < secretNum) {
-            System.out.println("Число " + player.getNum() + " игрока " + player.getName() +
+                    "Количество попыток осталось - " + (9 - player.getAttempt()));
+            player.incrementAttempt();
+        } else if (playerNum < secretNum) {
+            System.out.println("Число " + playerNum + " игрока " + player.getName() +
                     " меньше того, что загадал компьютер\n" +
-                    "Количество попыток осталось - " + (9 - player.getIndexArray()));
-        }
-        if (player.getIndexArray() == 9) {
-            System.out.println("Вы проиграли ваши количество попыток 0");
+                    "Количество попыток осталось - " + (9 - player.getAttempt()));
+            player.incrementAttempt();
+        } else {
+            System.out.println("Игрок " + player.getName() +
+                    " угадал число " + secretNum + " с " + (player.getAttempt() + 1) + " попытки");
+            player.incrementAttempt();
+            player.addNum(playerNum);
+            return false;
         }
         return true;
     }
 
-    private void randomNum() {
-        secretNum = (int) (Math.random() * 100) + 1;
-    }
-
     public void printNums(Player player) {
         System.out.print("Попытки игрока " + player.getName() + " ");
-        int[] newNums = player.copyNums();
-        for (int i : newNums) {
+        for (int i : player.copyNums()) {
             System.out.print(i + " ");
         }
         System.out.println();
--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -5,41 +5,23 @@ import java.util.Arrays;
 class Player {
 
     private String name;
-
+    private int attempt;
     private int[] nums = new int[10];
 
-    private int attempt = 0;
+    public Player(String name) { this.name = name; }
+
+    public String getName() { return name; }
+
+    public int[] copyNums() { return Arrays.copyOf(nums, attempt); }
+
+    public void incrementAttempt() { attempt++; }
+
+    public int getAttempt() { return attempt; }
 
     public void clear() {
         Arrays.fill(nums, 0, attempt, 0);
         attempt = 0;
     }
 
-    public int[] copyNums() {
-        return nums;
-    }
-
-    public Player(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getNum() {
-        return nums[attempt];
-    }
-
-    public int getIndexArray() { return attempt;}
-
-    public boolean addNum(int num) {
-        if((num >= 1 && num <= 100)) {
-            nums[attempt] = num;
-            attempt++;
-            return true;
-         }
-        System.out.println("Ошибка введено число меньше 1 или больше 100 попробуйте снова");
-        return false; 
-    }
+    public void addNum(int num) { nums[attempt] = num; }
 }
